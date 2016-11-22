f529441c879c98fca277133dc00308a2eb439735
David <entvex@gmail.com>
2016-11-22 09:14:14 +0100

    changed ts

--- opgave3Fitness/src/app/addworkout-list/addworkout-list.component.ts
+++ opgave3Fitness/src/app/addworkout-list/addworkout-list.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-addworkout-list',
   templateUrl: './addworkout-list.component.html',
   styleUrls: ['./addworkout-list.component.css']
 })
-export class AddworkoutListComponent implements OnInit {
+export class AddworkoutListComponent implements OnInit,OnDestroy {
+
+  private subscription : Subscription;
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 
   constructor(private route: ActivatedRoute) { }
-
 
   numberOfSets = [1,2,3,4,5,6,7,8,9,10];
   numberOfReps = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,30,45,60];
   repType = ["reps","min","sec"];
-  number : String;
+  number : Number;
 
   ngOnInit() {
-    this.number = this.route.queryParams['workoutNo'];
-    console.log(this.number);
+    this.subscription = this.route.params.subscribe(
+      (param: any) => {
+        this.number = param["workoutNo"];
+      }
+    )
   }
-
 }
