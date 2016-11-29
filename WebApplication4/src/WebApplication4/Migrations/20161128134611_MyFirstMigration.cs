26bb2f4c70838a8decdb81b980cc49ec97739ba3
David <entvex@gmail.com>
2016-11-29 07:57:05 +0100

    added stuff

---
+++ WebApplication4/src/WebApplication4/Migrations/20161128134611_MyFirstMigration.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication4.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EsImage",
+                columns: table => new
+                {
+                    ESImageId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ImageData = table.Column<byte[]>(nullable: true),
+                    ImageMimeType = table.Column<string>(maxLength: 128, nullable: true),
+                    Thumbnail = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EsImage", x => x.ESImageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComponentType",
+                columns: table => new
+                {
+                    ComponentTypeId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AdminComment = table.Column<string>(nullable: true),
+                    ComponentInfo = table.Column<string>(nullable: true),
+                    ComponentName = table.Column<string>(nullable: true),
+                    Datasheet = table.Column<string>(nullable: true),
+                    ImageESImageId = table.Column<long>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Manufacturer = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    WikiLink = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComponentType", x => x.ComponentTypeId);
+                    table.ForeignKey(
+                        name: "FK_ComponentType_EsImage_ImageESImageId",
+                        column: x => x.ImageESImageId,
+                        principalTable: "EsImage",
+                        principalColumn: "ESImageId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ComponentTypeId = table.Column<long>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.CategoryId);
+                    table.ForeignKey(
+                        name: "FK_Category_ComponentType_ComponentTypeId",
+                        column: x => x.ComponentTypeId,
+                        principalTable: "ComponentType",
+                        principalColumn: "ComponentTypeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Component",
+                columns: table => new
+                {
+                    ComponentId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AdminComment = table.Column<string>(nullable: true),
+                    ComponentNumber = table.Column<int>(nullable: false),
+                    ComponentTypeId = table.Column<long>(nullable: false),
+                    CurrentLoanInformationId = table.Column<long>(nullable: true),
+                    SerialNo = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    UserComment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Component", x => x.ComponentId);
+                    table.ForeignKey(
+                        name: "FK_Component_ComponentType_ComponentTypeId",
+                        column: x => x.ComponentTypeId,
+                        principalTable: "ComponentType",
+                        principalColumn: "ComponentTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryComponentType",
+                columns: table => new
+                {
+                    ComponentTypesId = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false),
+                    ComponentTypeId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryComponentType", x => new { x.ComponentTypesId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_CategoryComponentType_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryComponentType_ComponentType_ComponentTypeId",
+                        column: x => x.ComponentTypeId,
+                        principalTable: "ComponentType",
+                        principalColumn: "ComponentTypeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_ComponentTypeId",
+                table: "Category",
+                column: "ComponentTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Component_ComponentTypeId",
+                table: "Component",
+                column: "ComponentTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComponentType_ImageESImageId",
+                table: "ComponentType",
+                column: "ImageESImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryComponentType_CategoryId",
+                table: "CategoryComponentType",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryComponentType_ComponentTypeId",
+                table: "CategoryComponentType",
+                column: "ComponentTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Component");
+
+            migrationBuilder.DropTable(
+                name: "CategoryComponentType");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "ComponentType");
+
+            migrationBuilder.DropTable(
+                name: "EsImage");
+        }
+    }
+}
